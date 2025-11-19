d4e6f0062de2cdd393b794c31153b9c2739739a3
GDIsanto <logantom85@gmail.com>
2025-11-19 11:16:52 +0100

    completato esercizio con condizione switch

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
 let voto = parseInt(prompt(`inserisci il voto`));
 
-if(voto < 18 && voto >= 0){
-    console.log(`insufficiente`);
-}else if(voto >= 18 && voto < 21){
-    console.log(`sufficiente`);
-}else if(voto >= 21 && voto < 24){
-    console.log(`buono`);
-}else if(voto >= 24 && voto < 27){
-    console.log(`distinto`);
-}else if(voto >= 27 && voto <= 29){
-    console.log(`ottimo`);
-}else if(voto == 30){
-    console.log(`eccellente`);
-}else{
-    console.log(`voto non valido`);    
-};
+// if(voto < 18 && voto >= 0){
+//     console.log(`insufficiente`);
+// }else if(voto >= 18 && voto < 21){
+//     console.log(`sufficiente`);
+// }else if(voto >= 21 && voto < 24){
+//     console.log(`buono`);
+// }else if(voto >= 24 && voto < 27){
+//     console.log(`distinto`);
+// }else if(voto >= 27 && voto <= 29){
+//     console.log(`ottimo`);
+// }else if(voto == 30){
+//     console.log(`eccellente`);
+// }else{
+//     console.log(`voto non valido`);    
+// };
 
+
+switch (true) {
+    case voto >= 0 && voto < 18:
+        console.log(`insufficiente`);
+        break;
+    case voto >= 18 && voto < 21:
+        console.log(`sufficiente`);
+        break;
+    case voto >= 21 && voto < 24:
+        console.log(`buono`);
+        break;
+    case voto >= 24 && voto < 27:
+        console.log(`distinto`);
+        break;
+    case voto >= 27 && voto <= 29:
+        console.log(`ottimo`);
+        break;
+    case voto == 30:
+        console.log(`eccellente`);
+        break;
+    default:
+        console.log(`voto non valido`);
+}
