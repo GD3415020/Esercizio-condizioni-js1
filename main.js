5ecfcd372ed8e97e899a54d656863dd0698c3dd4
GDIsanto <logantom85@gmail.com>
2025-11-19 11:02:42 +0100

    completato esercizio con condizione if-else

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log(`ciao`);
+let voto = parseInt(prompt(`inserisci il voto`));
+
+if(voto < 18 && voto >= 0){
+    console.log(`insufficiente`);
+}else if(voto >= 18 && voto < 21){
+    console.log(`sufficiente`);
+}else if(voto >= 21 && voto < 24){
+    console.log(`buono`);
+}else if(voto >= 24 && voto < 27){
+    console.log(`distinto`);
+}else if(voto >= 27 && voto <= 29){
+    console.log(`ottimo`);
+}else if(voto == 30){
+    console.log(`eccellente`);
+}else{
+    console.log(`voto non valido`);    
+};
+
